36bfe504fb71eb4ef883cc09d7e41fb72c6a9e41
root <root@debian>
2015-07-04 13:00:07 +0200

    new file:   build.xml 	new file:   build/built-jar.properties 	new file:   build/classes/.netbeans_automatic_build 	new file:   build/classes/.netbeans_update_resources 	new file:   build/classes/cesar_cipher/Cesar_cipher.class 	new file:   build/classes/cesar_cipher/Excepecion$1.class 	new file:   build/classes/cesar_cipher/Excepecion.class 	new file:   build/classes/cesar_cipher/Excepecion.form 	new file:   build/classes/cesar_cipher/Thumbs.db 	new file:   build/classes/cesar_cipher/Ventana$1.class 	new file:   build/classes/cesar_cipher/Ventana$2.class 	new file:   build/classes/cesar_cipher/Ventana$3.class 	new file:   build/classes/cesar_cipher/Ventana$4.class 	new file:   build/classes/cesar_cipher/Ventana$5.class 	new file:   build/classes/cesar_cipher/Ventana.class 	new file:   build/classes/cesar_cipher/Ventana.form 	new file:   build/classes/cesar_cipher/images.jpg 	new file:   dist/README.TXT 	new file:   dist/Vigenere_cipher_with_numbers.jar 	new file:   manifest.mf 	new file:   nbproject/build-impl.xml 	new file:   nbproject/genfiles.properties 	new file:   nbproject/private/config.properties 	new file:   nbproject/private/private.properties 	new file:   nbproject/private/private.xml 	new file:   nbproject/project.properties 	new file:   nbproject/project.xml 	new file:   src/cesar_cipher/Cesar_cipher.java 	new file:   src/cesar_cipher/Excepecion.form 	new file:   src/cesar_cipher/Excepecion.java 	new file:   src/cesar_cipher/Thumbs.db 	new file:   src/cesar_cipher/Ventana.form 	new file:   src/cesar_cipher/Ventana.java 	new file:   src/cesar_cipher/images.jpg

---
+++ src/cesar_cipher/Cesar_cipher.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cesar_cipher;
+/**
+ *
+ * @author ivan
+ */
+public class Cesar_cipher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana ventana=new Ventana();
+        ventana.setSize(780, 560);
+        ventana.setVisible(true);
+    }
+    
+}
